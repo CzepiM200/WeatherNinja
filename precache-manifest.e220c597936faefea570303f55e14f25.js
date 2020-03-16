9d080c55ccc051ac06802c220079a44b04e6e071
Marcin Czepiela <czepimm200@gmail.com>
2020-03-16 02:07:37 +0100

    Updates

---
+++ precache-manifest.e220c597936faefea570303f55e14f25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29c9dedf5b2a719d351d5a20b929ea36",
+    "url": "/WeatherNinja/index.html"
+  },
+  {
+    "revision": "0a733c663e0ea17c6d12",
+    "url": "/WeatherNinja/static/css/main.781ff085.chunk.css"
+  },
+  {
+    "revision": "c347c8a2c139e01c958c",
+    "url": "/WeatherNinja/static/js/2.cc3ac117.chunk.js"
+  },
+  {
+    "revision": "d2cf8d37946cd02abf2fe73b5ccb09c4",
+    "url": "/WeatherNinja/static/js/2.cc3ac117.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a733c663e0ea17c6d12",
+    "url": "/WeatherNinja/static/js/main.d51f08a4.chunk.js"
+  },
+  {
+    "revision": "3c4df7084fc7a2439433",
+    "url": "/WeatherNinja/static/js/runtime-main.c1945513.js"
+  }
+]);
